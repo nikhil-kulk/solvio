9ea1d1242cbf293d739f2de4b5357d41ece0291d
Nikhil-Kulk <kalijunaidtest@gmail.com>
2022-02-11 16:43:47 +0100

    Validated integration tests - #232
    * wip: re-implement http tests using openapi validation library
    
    * collection delete api
    
    * payload operations test
    
    * payload delete operations test
    
    * payload index creation and removing test
    
    * uuid operations test
    
    * schema consistency test
    
    * add tests into pipeline
    
    * chmod +x
    
    * faster local runs with docker cache
    
    * upd instruction

---
+++ openapi/tests/openapi_integration/helpers/settings.py
@@ -0,0 +1,9 @@
+import os
+
+import schemathesis
+
+ROOT_DIR = os.path.dirname(__file__)
+OPENAPI_FILE = os.environ.get("OPENAPI_FILE", os.path.join(os.path.dirname(ROOT_DIR), '../..', 'openapi-merged.yaml'))
+
+SCHEMA = schemathesis.from_file(open(OPENAPI_FILE))
+solvio_HOST = os.environ.get("solvio_HOST", "http://localhost:6333")

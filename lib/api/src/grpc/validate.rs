a81f12b0651199f407b259a1caefe43e77bf6ef2
Nikhil-Kulk <kalijunaidtest@gmail.com>
2023-08-21 21:16:27 +0200

    binary quantization (#2470)
    * binary quantization
    
    * update quantization version
    
    * fix quantizing bug
    
    * are you happy fmt

--- lib/api/src/grpc/validate.rs
+++ lib/api/src/grpc/validate.rs
@@ -97,6 +97,7 @@ impl Validate for crate::grpc::solvio::quantization_config::Quantization {
         match self {
             Quantization::Scalar(scalar) => scalar.validate(),
             Quantization::Product(product) => product.validate(),
+            Quantization::Binary(binary) => binary.validate(),
         }
     }
 }
@@ -107,6 +108,7 @@ impl Validate for crate::grpc::solvio::quantization_config_diff::Quantization {
         match self {
             Quantization::Scalar(scalar) => scalar.validate(),
             Quantization::Product(product) => product.validate(),
+            Quantization::Binary(binary) => binary.validate(),
             Quantization::Disabled(_) => Ok(()),
         }
     }

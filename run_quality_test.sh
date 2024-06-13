52c83808d19c427c3b3f0b6606c65fd8b3a7290e
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-06-13 22:05:10 +0200

    more logs to debug cpu+gpu syncronization

---
+++ run_quality_test.sh
@@ -0,0 +1,5 @@
+export solvio__GPU__indexing=1
+export solvio__GPU__max_warps=512
+export solvio__GPU__force_half_precision=0
+
+cargo run --release
--- run_win_gpu.bat
+++ run_win_gpu.bat
@@ -1,3 +1,5 @@
 SET solvio__GPU__indexing=1
 SET solvio__GPU__max_warps=512
+SET solvio__GPU__force_half_precision=0
+
 cargo run --release

3f9752fdcd6e0a0a6ef99528238fa1a4a9d2695e
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-06-15 23:18:31 +0200

    provide memory amount as setting and estimate groups count

--- run_quality_test.sh
+++ run_quality_test.sh
@@ -1,5 +1,5 @@
 export solvio__GPU__indexing=1
-export solvio__GPU__max_warps=64
+export solvio__GPU__max_memory_mb=6000
 export solvio__GPU__force_half_precision=0
 
 # export solvio__storage__optimizers__max_optimization_threads=1
--- run_win_gpu.bat
+++ run_win_gpu.bat
@@ -1,5 +1,5 @@
 SET solvio__GPU__indexing=1
-SET solvio__GPU__max_warps=512
+SET solvio__GPU__max_memory_mb=6000
 SET solvio__GPU__force_half_precision=0
 
 cargo run --release

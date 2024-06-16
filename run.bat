cc879c72b0265cd2fa618f1bfe2bdaa15d0cedad
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-06-16 23:31:31 +0200

    revert max groups settings

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 SET solvio__GPU__indexing=1
-SET solvio__GPU__max_memory_mb=5000
+SET solvio__GPU__max_groups=2000
 SET solvio__GPU__force_half_precision=0
 
 SET solvio__storage__optimizers__max_optimization_threads=1
--- run_quality_test.sh
+++
@@ -1,7 +0,0 @@
-export solvio__GPU__indexing=1
-export solvio__GPU__max_memory_mb=6000
-export solvio__GPU__force_half_precision=0
-
-# export solvio__storage__optimizers__max_optimization_threads=1
-
-cargo run --release

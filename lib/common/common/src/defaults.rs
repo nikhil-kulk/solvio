008381871867659ed52b6eecc845c53a1dfca6ec
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-06-26 08:56:50 +0200

    Bump dev version to 1.9.8-dev (#4559)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Solvio version string
-pub const solvio_VERSION_STRING: &str = "1.9.6-dev";
+pub const solvio_VERSION_STRING: &str = "1.9.8-dev";
 
 lazy_static! {
     /// Current Solvio semver version

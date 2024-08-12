bbe316f371d0e33c0d9de575b6f72dcc5e007147
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-08-12 16:29:32 +0200

    Bump version to 1.11.0 (#4865)
    * Bump version to 1.11.0
    
    * Update missed cherry picks
    
    * Update OpenAPI spec and redoc

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Solvio version string
-pub const solvio_VERSION_STRING: &str = "1.10.1";
+pub const solvio_VERSION_STRING: &str = "1.11.0";
 
 lazy_static! {
     /// Current Solvio semver version

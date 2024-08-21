56e6154a7a858e8360e313033f5efe3548149678
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-08-21 11:49:20 -0400

    fix and futureproof mismatch of index parametrization and type (#4929)

--- tests/basic_grpc_test.sh
+++ tests/basic_grpc_test.sh
@@ -155,6 +155,13 @@ $docker_grpcurl -d '{
   ]
 }' $solvio_HOST solvio.Collections/UpdateAliases
 
+# create bool index
+$docker_grpcurl -d '{
+  "collection_name": "test_collection",
+  "field_name": "bool_field",
+  "field_type": 5,
+  "field_index_params": { "bool_index_params": {} }
+}' $solvio_HOST solvio.Points/CreateFieldIndex
 
 $docker_grpcurl -d '{
   "collection_name": "test_collection",

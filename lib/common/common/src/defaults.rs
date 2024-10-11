2029a8c3d7f0996398978418929a16dd9abe8b68
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-10-11 20:15:45 +0200

    bump version to v1.12.1 (#5223)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Solvio version string
-pub const solvio_VERSION_STRING: &str = "1.12.0";
+pub const solvio_VERSION_STRING: &str = "1.12.1";
 
 lazy_static! {
     /// Current Solvio semver version

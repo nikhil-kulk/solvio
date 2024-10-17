1a0b6a086593003926f0fa5640d56d1043c7a23c
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-10-17 12:28:48 +0200

    Do not create errors eagerly (#5254)

--- lib/collection/src/operations/conversions.rs
+++ lib/collection/src/operations/conversions.rs
@@ -1141,7 +1141,7 @@ impl TryFrom<api::grpc::solvio::TargetVector> for RecommendExample {
     fn try_from(value: api::grpc::solvio::TargetVector) -> Result<Self, Self::Error> {
         value
             .target
-            .ok_or(Status::invalid_argument("Target vector is malformed"))
+            .ok_or_else(|| Status::invalid_argument("Target vector is malformed"))
             .and_then(|target| match target {
                 api::grpc::solvio::target_vector::Target::Single(vector_example) => {
                     Ok(vector_example.try_into()?)
@@ -1228,7 +1228,7 @@ impl TryFrom<api::grpc::solvio::CoreSearchPoints> for CoreSearchRequest {
                 })
             })
             .transpose()?
-            .ok_or(Status::invalid_argument("Query is not specified"))?;
+            .ok_or_else(|| Status::invalid_argument("Query is not specified"))?;
 
         Ok(Self {
             query,
@@ -1337,9 +1337,11 @@ impl TryFrom<api::grpc::solvio::VectorExample> for RecommendExample {
     fn try_from(value: api::grpc::solvio::VectorExample) -> Result<Self, Self::Error> {
         value
             .example
-            .ok_or(Status::invalid_argument(
-                "Vector example, which can be id or bare vector, is malformed",
-            ))
+            .ok_or_else(|| {
+                Status::invalid_argument(
+                    "Vector example, which can be id or bare vector, is malformed",
+                )
+            })
             .and_then(|example| match example {
                 api::grpc::solvio::vector_example::Example::Id(id) => {
                     Ok(Self::PointId(id.try_into()?))
@@ -1688,7 +1690,7 @@ impl TryFrom<api::grpc::solvio::CreateShardKey> for CreateShardingKey {
             shard_key: op
                 .shard_key
                 .and_then(convert_shard_key_from_grpc)
-                .ok_or(Status::invalid_argument("Shard key is not specified"))?,
+                .ok_or_else(|| Status::invalid_argument("Shard key is not specified"))?,
             shards_number: op
                 .shards_number
                 .map(NonZeroU32::try_from)
@@ -1721,7 +1723,7 @@ impl TryFrom<api::grpc::solvio::DeleteShardKey> for DropShardingKey {
             shard_key: op
                 .shard_key
                 .and_then(convert_shard_key_from_grpc)
-                .ok_or(Status::invalid_argument("Shard key is not specified"))?,
+                .ok_or_else(|| Status::invalid_argument("Shard key is not specified"))?,
         })
     }
 }

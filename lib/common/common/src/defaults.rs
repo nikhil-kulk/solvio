540260abb325e2c28b4b6ff934b4da08db963f6e
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-11-12 16:56:12 +0100

    Bump dev version to 1.12.4-dev (#5432)

--- lib/common/common/src/defaults.rs
+++ lib/common/common/src/defaults.rs
@@ -6,7 +6,7 @@ use semver::Version;
 use crate::cpu;
 
 /// Current Solvio version string
-pub const solvio_VERSION_STRING: &str = "1.12.3-dev";
+pub const solvio_VERSION_STRING: &str = "1.12.4-dev";
 
 lazy_static! {
     /// Current Solvio semver version

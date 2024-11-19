1bb4fab967fa69a5738328f99e96e57fe6df0e04
Nikhil-Kulk <kalijunaidtest@gmail.com>
2024-11-19 16:46:02 +0100

    Do not build schema_generator by default (#5482)

--- tools/generate_openapi_models.sh
+++ tools/generate_openapi_models.sh
@@ -33,7 +33,7 @@ sh_with_ytt '
 '
 
 # Generates models from internal service structures
-cargo run --package solvio --bin schema_generator > ./openapi/schemas/AllDefinitions.json
+cargo run --package solvio --features="service_debug" --bin schema_generator > ./openapi/schemas/AllDefinitions.json
 
 docker build tools/schema2openapi --tag schema2openapi
 
